3758024e4c2aad085f13453101d7265013c61076
Fady Samir <37264344+Fady50@users.noreply.github.com>
2018-09-25 13:20:54 -0700

    first commit

--- js/script.js
+++ js/script.js
@@ -21,6 +21,7 @@ $(document).ready(function(){
 			});
 		}
 	});
+
 	// Price Table
 	var individual_price_table = $('#price_tables').find('.individual');
 	var company_price_table = $('#price_tables').find('.company');
@@ -44,46 +45,45 @@ $(document).ready(function(){
 		company_price_table.addClass('active');
 		individual_price_table.removeClass('active');
 	});
-		/*
-			===================== Scroll Smothy =====================
-		*/
-		// Select all links with hashes
-		$('a[href*="#"]')
-			// Remove links that don't actually link to anything
-			.not('[href="#"]')
-			.not('[href="#0"]')
-			.not('[href="#quote-carousel"]')
-			.click(function(event) {
-			// On-page links
-			if (
-			  location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-			  &&
-			  location.hostname == this.hostname
-			) {
-			  // Figure out element to scroll to
-			  var target = $(this.hash);
-			  target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-			  // Does a scroll target exist?
-			  if (target.length) {
-			    // Only prevent default if animation is actually gonna happen
-			    event.preventDefault();
-			    $('html, body').animate({
-			      scrollTop: target.offset().top
-			    }, 1000, function() {
-			      // Callback after animation
-			      // Must change focus!
-			      var $target = $(target);
-			      $target.focus();
-			      if ($target.is(":focus")) { // Checking if the target was focused
-			        return false;
-			      } else {
-			        $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-			        $target.focus(); // Set focus again
-			      };
-			    });
-			  }
+
+	/* Scroll Smoothy */
+	// Select all links with hashes
+	$('a[href*="#"]')
+		// Remove links that don't actually link to anything
+		.not('[href="#"]')
+		.not('[href="#0"]')
+		.not('[href="#quote-carousel"]')
+		.click(function(event) {
+		// On-page links
+		if(
+			location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+			&&
+			location.hostname == this.hostname
+		){
+			// Figure out element to scroll to
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			// Does a scroll target exist?
+			if (target.length) {
+				// Only prevent default if animation is actually gonna happen
+				event.preventDefault();
+				$('html, body').animate({
+					scrollTop: target.offset().top
+				}, 1000, function() {
+					// Callback after animation
+					// Must change focus!
+					var $target = $(target);
+					$target.focus();
+					if ($target.is(":focus")) { // Checking if the target was focused
+						return false;
+					} else {
+						$target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+						$target.focus(); // Set focus again
+					};
+				});
 			}
-		});
+		}
+	});
 
 	// Wow Animations
 	wow = new WOW(
@@ -99,5 +99,4 @@ $(document).ready(function(){
 
 	// Magnific Popup
 	$(".lightbox").magnificPopup();
-
 });
